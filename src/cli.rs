59071b349956c84d933b58341ccda7df36e4dc3a
Nawasan Wisitsingkhon <nawasan.dev@gmail.com>
2024-12-08 15:51:08 +0700

    add generate completion

--- src/cli.rs
+++ src/cli.rs
@@ -1,13 +1,19 @@
-use crate::gen_pass;
-use clap::{command, Parser, ValueEnum};
+use std::io;
 
-#[derive(Parser)]
-#[command(name = "tenjin",author, version, about, long_about = None)]
+use crate::gen_pass;
+use clap::{command, CommandFactory, Parser, ValueEnum};
+use clap_complete::{generate, Shell};
+
+#[derive(Parser, Default)]
+#[command(name = "fpas",author, version, about, long_about = None)]
 struct Cli {
     #[clap(short, long, default_value_t, value_enum)]
     /// Mode for generate password
     mode: Mode,
-    msg: String,
+    msg: Option<String>,
+    #[clap(long, short)]
+    /// genrate completions for any shell
+    completions: Option<Shell>,
 }
 
 #[derive(Clone, Debug, Default, ValueEnum)]
@@ -21,9 +27,21 @@ enum Mode {
 
 pub fn system() {
     let cli = Cli::parse();
-    let raw_hash = match cli.mode {
-        Mode::B | Mode::Byte => gen_pass::byte_mode(cli.msg),
-        Mode::N | Mode::Normal => gen_pass::normal_mode(cli.msg),
-    };
-    println!("{}", raw_hash);
+
+    if let Some(shell) = cli.completions {
+        let mut cli_gen = Cli::command();
+        generate(shell, &mut cli_gen, "fpas", &mut io::stdout());
+        return;
+    }
+
+    if let Some(msg) = cli.msg {
+        let raw_hash = match cli.mode {
+            Mode::B | Mode::Byte => gen_pass::byte_mode(msg),
+            Mode::N | Mode::Normal => gen_pass::normal_mode(msg),
+        };
+        println!("{}", raw_hash);
+    } else {
+        // show help 
+        Cli::parse_from(&[std::env::args().nth(0).unwrap().as_str(), "--help"]);
+    }
 }
